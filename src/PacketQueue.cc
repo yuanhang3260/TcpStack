0654db70f2f48e06adb89e2735ebf3d59cc28daf
Hang Yuan <yuanhang3260@gmail.com>
2017-03-29 17:45:53 -0700

    a safe blocking queue?

--- src/PacketQueue.cc
+++ src/PacketQueue.cc
@@ -1,12 +1,26 @@
+#include <thread>
+
 #include "PacketQueue.h"
 
 namespace net_stack {
 
-void PacketQueue::Push(std::unique_ptr<Packet> new_ele) {
+PacketQueue::PacketQueue() {
+  destroy_.store(false);
+}
+
+PacketQueue::~PacketQueue() {
+  destroy_.store(true);
+  cv_.notify_all();
+}
+
+bool PacketQueue::Push(std::unique_ptr<Packet> new_ele) {
+  std::unique_lock<std::mutex> lock(mutex_);
   packets_.push(std::move(new_ele));
+  return true;
 }
 
 uint32 PacketQueue::Push(std::queue<std::unique_ptr<Packet>>* pkts) {
+  std::unique_lock<std::mutex> lock(mutex_);
   uint32 size = pkts->size();
   for (uint32 i = 0; i < size; i++) {
     packets_.push(std::move(pkts->front()));
@@ -15,24 +29,60 @@ uint32 PacketQueue::Push(std::queue<std::unique_ptr<Packet>>* pkts) {
   return size;
 }
 
-std::unique_ptr<Packet> PacketQueue::DeQueue() {
+std::unique_ptr<Packet> PacketQueue::DeQueue(bool blocking) {
+  std::unique_lock<std::mutex> lock(mutex_);
+  if (!blocking) {
+    if (packets_.empty()) {
+      return nullptr;
+    }
+  } else {
+    cv_.wait(lock, [this] { return destroy_.load() || !packets_.empty(); });
+  }
+
+  if (destroy_.load()) {
+    return nullptr;
+  }
+
+  if (packets_.empty()) {
+    return nullptr;
+  }
+
   auto re = std::move(packets_.front());
   packets_.pop();
   return re;
 }
 
 uint32 PacketQueue::DeQueueAllTo(
-    std::queue< std::unique_ptr<Packet> >* receiver_queue) {
+    std::queue< std::unique_ptr<Packet> >* receiver_queue, bool blocking) {
+  std::unique_lock<std::mutex> lock(mutex_);
+  if (!blocking) {
+    if (packets_.empty()) {
+      return 0;
+    }
+  } else {
+    cv_.wait(lock, [this] { return destroy_.load() || !packets_.empty(); });
+  }
+
+  if (destroy_.load()) {
+    return 0;
+  }
+
+  if (packets_.empty()) {
+    return 0;
+  }
+
   uint32 total_size = packets_.size();
   receiver_queue->swap(packets_);
   return total_size;
 }
 
-uint32 PacketQueue::size() const {
+uint32 PacketQueue::size() {
+  std::unique_lock<std::mutex> lock(mutex_);
   return packets_.size();
 }
 
-bool PacketQueue::empty() const {
+bool PacketQueue::empty() {
+  std::unique_lock<std::mutex> lock(mutex_);
   return packets_.empty();
 }
 
--- src/PacketQueue.h
+++ src/PacketQueue.h
@@ -1,7 +1,11 @@
 #ifndef NET_STACK_PACKET_QUEUE_
 #define NET_STACK_PACKET_QUEUE_
 
+#include <atomic>
+#include <condition_variable>
+#include <functional>
 #include <memory>
+#include <mutex>
 #include <queue>
 
 #include "Base/BaseTypes.h"
@@ -9,22 +13,31 @@
 
 namespace net_stack {
 
-// This class wrapps a queue of Packet. This class is not thread-safe.
+// This class wrapps a queue of Packet. This class is thread-safe. It supports
+// blocking and non-blocking reading.
 class PacketQueue {
  public:
-  PacketQueue() = default;
+  PacketQueue();
+  ~PacketQueue();
 
-  void Push(std::unique_ptr<Packet> ele);
+  bool Push(std::unique_ptr<Packet> ele);
   uint32 Push(std::queue<std::unique_ptr<Packet>>* pkts);
 
-  std::unique_ptr<Packet> DeQueue();
-  uint32 DeQueueAllTo(std::queue< std::unique_ptr<Packet> >* receiver_queue);
+  // In non-blocking mode, if queue is empty, imediately return nullptr.
+  std::unique_ptr<Packet> DeQueue(bool blocking = true);
+  // In non-blocking mode, if queue is empty, immediately return 0.
+  uint32 DeQueueAllTo(std::queue< std::unique_ptr<Packet> >* receiver_queue,
+                      bool blocking = true);
 
-  uint32 size() const;
-  bool empty() const;
+  uint32 size();
+  bool empty();
 
  private:
   std::queue<std::unique_ptr<Packet>> packets_;
+  std::mutex mutex_;
+  std::condition_variable cv_;
+
+  std::atomic_bool destroy_;
 };
 
 }  // namespace net_stack
